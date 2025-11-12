83f64e02bf88bea0fec255b403760f126429199f
R9950X_Sudoer <hhj@sudoer.cn>
2025-11-12 21:56:44 +0800

    添加get_page_count

--- NGAClient.py
+++ NGAClient.py
@@ -2,7 +2,7 @@ import json
 import config
 import requests
 
-
+#我们这里假定脚本运行时间很短，在一次运行中帖子不会发生变化
 class NGAClient:
     def __init__(self):
         self.session = requests.Session()
@@ -13,12 +13,24 @@ class NGAClient:
             }
         )
         self.base_url = config.BASE_URL
+        self.page_cache = {}
+
+    def page_cache_key(self, tid, aid, page):
+        return f"{tid}_{aid if aid else 'all'}_page_{page}"
+
+    def get_page_count(self, tid, aid):
+        first_page_data = self.get_page(tid, aid, 1)
+        total_pages = first_page_data.get("totalPage", 1)
+        return total_pages
 
     def get_page(self, tid, aid, page):
         if not tid and not page:
             raise ValueError("Either tid or page must be provided.")
         if page < 1:
             raise ValueError("Page number must be greater than 0.")
+        cache_key = self.page_cache_key(tid, aid, page)
+        if cache_key in self.page_cache:
+            return self.page_cache[cache_key]
         """
         go代码：
         {
@@ -44,17 +56,7 @@ class NGAClient:
         if json_data.get("code")!=0:
             raise Exception(f"Error fetching page: {json_data.get('msg', 'Unknown error')}")
 
-        #保存到tmp.json
-        #with open("tmp.json", "w", encoding="utf-8") as f:
-        #    json.dump(json_data, f, ensure_ascii=False, indent=4)
-
+        self.page_cache[cache_key] = json_data
         return json_data
 
-    def get_ngahtml(self,tid,aid,page):
-        if not tid and not page:
-            raise ValueError("Either pid or page must be provided.")
-        if page < 1:
-            raise ValueError("Page number must be greater than 0.")
-        url = f"{self.base_url}/read.php?tid={tid}&authorid={aid}&page={page}&rand=556"
-        response = self.session.get(url)
-        response.raise_for_status()
+    
